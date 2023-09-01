453ea3d94c6431e48bd4f59ef4dd5fd87b5cf874
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-09-01 12:19:44 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='steps'>
-
+      <div className="steps">
+        <div className="numbers">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <p className="word"> Step 1:Learn React</p>
+        <div className="buttons">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
       </div>
     </div>
   );
