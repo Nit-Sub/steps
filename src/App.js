df9e55ba7037bf85eb85a4d271093056f4ead9b9
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-09-03 09:34:08 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 
 function App() {
+  const messages =[
+    "Learn React",
+    "Apply for Jobs",
+    "Invest your new income"
+  ]
+  const step=1;
+  const handlePrevious=()=>{
+    alert("Previous")
+  }
+  const handleNext=()=>{
+    alert("Next")
+  }
   return (
       <div className="steps">
         <div className="numbers">
-          <div className="active">1</div>
-          <div>2</div>
-          <div>3</div>
+          <div className={`${step>=1 ?"active" :""}`}>1</div>
+          <div className={`${step>=2 ?"active" :""}`}> 2</div>
+          <div className={`${step>=3 ?"active" :""}`}>3</div>
         </div>
-        <p className="message"> Step 1:Learn React</p>
+        <p className="message">{step}:{messages[step-1]}</p>
         <div className="buttons">
-          <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Previous</button>
-          <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Next</button>
+          <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handlePrevious}>Previous</button>
+          <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handleNext}>Next</button>
         </div>
       </div>
   );
