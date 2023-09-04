b6a5ea124fbf2b8aed4e4b2a41b833414afef08c
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-09-04 10:10:08 +0545

    .

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     "Invest your new income"
   ]
   const [step , setStep]=useState(1)
+  const [showhide, setShowhide ]=useState(true);
 
   const handlePrevious=()=>{
     if (step>1) setStep(step- 1)
@@ -16,19 +17,28 @@ function App() {
   const handleNext=()=>{
     if (step<3) setStep(step+1)
   }
+console.log(showhide)
   return (
-      <div className="steps">
-        <div className="numbers">
-          <div className={step>=1 ?"active" :""}>1</div>
-          <div className={step>=2 ?"active" :""}> 2</div>
-          <div className={step>=3 ?"active" :""}>3</div>
-        </div>
-        <p className="message">{step}:{messages[step-1]}</p>
-        <div className="buttons">
-          <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handlePrevious}>Previous</button>
-          <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handleNext}>Next</button>
-        </div>
-      </div>
+    <><div>
+      <button  className="close"onClick={()=>setShowhide(!showhide)}>&times;</button>
+   {
+    showhide &&
+    <div className="steps">
+    <div className="numbers">
+      <div className={step>=1 ?"active" :""}>1</div>
+      <div className={step>=2 ?"active" :""}> 2</div>
+      <div className={step>=3 ?"active" :""}>3</div>
+    </div>
+    <p className="message">{step}:{messages[step-1]}</p>
+    <div className="buttons">
+      <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handlePrevious}>Previous</button>
+      <button style={{backgroundColor:"#7950f2", color:"#fff"}} onClick={handleNext}>Next</button>
+    </div>
+  </div>
+
+   }
+   </div>
+      </>
   );
 }
 export default App;
