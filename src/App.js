e6af0a301abaed29612cb57e4277b92d4d2d6871
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-09-06 12:28:58 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import First from "./first.js";
+//import First from "./first.js";
 import Counter from "./counter.js";
 
 function App() {
   return(
-    <><First /><Counter /></>
+    <><Counter /></>
   )
  }
 
--- src/counter.js
+++ src/counter.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import './counter.css';
 const Counter = () => {
+    const [step, setStep] = useState(1)
+    const [count , setCount] = useState(1)
+    const handleminusbutton = () => {
+            setStep(step-1)
+    }
+    const handleplusbutton = () => {
+        setStep(step+1)
+}
     return (
         <>
-
-        </>
-    )
+            <div className='main'>
+                <div className='body'>
+                    <button onClick={handleminusbutton}>-</button>
+                    <p>Step: {step}</p>
+                    <button onClick={handleplusbutton}>+</button>
+                </div>
+                <div className='body'>
+                    <button>-</button>
+                    <p>count: {count}</p>
+                    <button>+</button>
+                </div>
+                <p>5 days from today is Sat jun 26 2027</p>
+            </div>
+        </>)
 }
 
 export default Counter;
